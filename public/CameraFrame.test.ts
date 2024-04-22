bfa6fbe45ec2c892d451fccd2e2cbc2739c24cb0
Joe <joe.cardwell@gmail.com>
2024-04-22 14:42:50 -0400

    Add tests for new camera frame

--- public/CameraFrame.test.ts
+++ public/CameraFrame.test.ts
@@ -1,4 +1,14 @@
-import CameraFrame, { Segment } from './CameraFrame.ts';
+import CameraFrame from "./CameraFrame";
+
+const jestConsole = console;
+
+beforeEach(() => {
+  global.console = require('console');
+});
+
+afterEach(() => {
+  global.console = jestConsole;
+});
 
 describe("CameraFrame.flip", () => {
   it("should not change an empty camera frame", () => {
@@ -19,5 +29,123 @@ describe("CameraFrame.flip", () => {
     frame.flip();
     expect(frame.segments[0].start).toBe(0.25);
     expect(frame.segments[0].end).toBe(0.75);
-  })
+  });
 });
+
+describe("CameraFrame.simplify", () => {
+  it("should not change a camera frame with only one segment", () => {
+    const frame = new CameraFrame();
+    frame.segments.push({
+      start: 0.25,
+      end: 0.75,
+      color: "red"
+    });
+    frame.simplify();
+    expect(frame.segments[0].start).toBe(0.25);
+    expect(frame.segments[0].end).toBe(0.75);
+  });
+
+  it("should connect two segments of the same colour", () => {
+    const frame = new CameraFrame();
+    frame.segments.push({
+      start: 0.25,
+      end: 0.5,
+      color: "red"
+    });
+    frame.segments.push({
+      start: 0.5,
+      end: 0.75,
+      color: "red"
+    });
+    frame.simplify();
+    expect(frame.segments[0].start).toBe(0.25);
+    expect(frame.segments[0].end).toBe(0.75);
+  });
+
+  it("should not connect two segments of different colours", () => {
+    const frame = new CameraFrame();
+    frame.segments.push({
+      start: 0.25,
+      end: 0.5,
+      color: "red"
+    });
+    frame.segments.push({
+      start: 0.5,
+      end: 0.75,
+      color: "blue"
+    });
+    frame.simplify();
+    expect(frame.segments[0].start).toBe(0.25);
+    expect(frame.segments[0].end).toBe(0.5);
+  });
+
+  it("should not connect non-adjacent segments", () => {
+    const frame = new CameraFrame();
+    frame.segments.push({
+      start: 0.25,
+      end: 0.4,
+      color: "red"
+    });
+    frame.segments.push({
+      start: 0.5,
+      end: 0.75,
+      color: "red"
+    });
+    frame.simplify();
+    expect(frame.segments.length).toBe(2);
+    expect(frame.segments[0].start).toBe(0.25);
+    expect(frame.segments[0].end).toBe(0.4);
+  });
+})
+
+describe("CameraFrame.compare", () => {
+  it("should return 100% similarity for comparing an empty frame", () => {
+    const frame = new CameraFrame();
+    const comparison = frame.compare(frame);
+    expect(comparison).toBe(1);
+  });
+
+  it("should return 50% similarity for a simple case", () => {
+    const frameA = new CameraFrame();
+    const frameB = new CameraFrame();
+    frameA.segments.push({
+      start: 0.25,
+      end: 0.75,
+      color: "red"
+    });
+    frameB.segments.push({
+      start: 0.25,
+      end: 0.75,
+      color: "blue"
+    });
+    const comparison = frameA.compare(frameB);
+    expect(comparison).toBe(0.5);
+  });
+
+  it("should properly create new breakpoints", () => {
+    /*
+    RRRRR-BBBB
+    ---BBBBB--
+    ffffffTTff
+    */
+    const frameA = new CameraFrame();
+    const frameB = new CameraFrame();
+    frameA.segments.push({
+      start: 0,
+      end: 0.5,
+      color: "red"
+    });
+    frameA.segments.push({
+      start: 0.6,
+      end: 1,
+      color: "blue"
+    });
+    frameB.segments.push({
+      start: 0.3,
+      end: 0.8,
+      color: "blue"
+    });
+    const comparison = frameA.compare(frameB);
+    expect(comparison).toBeCloseTo(0.2);
+  });
+})
--- public/CameraFrame.ts
+++ public/CameraFrame.ts
@@ -23,10 +23,59 @@ export default class CameraFrame {
   }
 
   simplify() {
+    const newSegments: Segment[] = [];
+    let lastColor = "none";
+    let lastEnd = 0;
+    this.segments.forEach((seg) => {
+      if (seg.color === lastColor && seg.start === lastEnd) {
+        newSegments[newSegments.length - 1].end = seg.end;
+      } else {
+        newSegments.push(seg);
+      }
+      lastColor = seg.color;
+      lastEnd = seg.end;
+    });
+    this.segments = newSegments;
+  }
 
+  at(position: number): string {
+    let ret = "none";
+    this.segments.forEach(seg => {
+      if (position >= seg.start && position < seg.end) {
+        ret = seg.color;
+      }
+    });
+    return ret;
   }
 
   compare(otherFrame: CameraFrame) {
-    
+    // find all breakpoints
+    const breakpoints = new Set<number>();
+    this.segments.forEach(seg => {
+      breakpoints.add(seg.start);
+      breakpoints.add(seg.end);
+    });
+    otherFrame.segments.forEach(seg => {
+      breakpoints.add(seg.start);
+      breakpoints.add(seg.end);
+    });
+    breakpoints.add(1);
+
+    const breakpointsArray = Array.from(breakpoints.values()).sort((a, b) => a - b);
+    const similarityZones: any[] = [];
+    let similarity = 0;
+    breakpointsArray.forEach((breakpoint, idx) => {
+      const start = (breakpointsArray[idx - 1]) || 0;
+      const position = (breakpoint + start) / 2;
+      const thisColor = this.at(position);
+      const otherColor = otherFrame.at(position);
+      similarityZones.push({
+        start,
+        end: breakpoint,
+        same: thisColor === otherColor
+      });
+      if (thisColor === otherColor) similarity += breakpoint - start;
+    });
+    return similarity;
   }
 }
