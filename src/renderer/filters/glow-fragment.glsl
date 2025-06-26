8a4efe8033d98f823d8882740d56a7870c128ffb
Joe <joe.cardwell@gmail.com>
2025-06-26 10:52:32 -0400

    Remove unused bits of shader

--- src/renderer/filters/glow-fragment.glsl
+++ src/renderer/filters/glow-fragment.glsl
@@ -107,19 +107,6 @@ float simplex3d(vec3 p) {
 	 return dot(d, vec4(52.0));
 }
 
-/* const matrices for 3d rotation */
-const mat3 rot1 = mat3(-0.37, 0.36, 0.85,-0.14,-0.93, 0.34,0.92, 0.01,0.4);
-const mat3 rot2 = mat3(-0.55,-0.39, 0.74, 0.33,-0.91,-0.24,0.77, 0.12,0.63);
-const mat3 rot3 = mat3(-0.71, 0.52,-0.47,-0.08,-0.72,-0.68,-0.7,-0.45,0.56);
-
-/* directional artifacts can be reduced by rotating each octave */
-float simplex3d_fractal(vec3 m) {
-    return   0.5333333*simplex3d(m*rot1)
-			+0.2666667*simplex3d(2.0*m*rot2)
-			+0.1333333*simplex3d(4.0*m*rot3)
-			+0.0666667*simplex3d(8.0*m);
-}
-
 float getNoise(vec2 fragCoord)
 {
 	vec2 p = fragCoord.xy/uInputSize.x;
@@ -141,8 +128,7 @@ bool isNG(vec4 color) {
 vec4 convolute(vec2 uv, bool noisy)
 {
     vec4 color = vec4(0);
-		int size = 10;
-		float pixelsInWindow = pow((float(size) * 2. + 1.), 2.);
+		int size = 15;
 		vec4 currentColor = texture(uTexture, vTextureCoord);
 		
 		if (!isNG(currentColor)) {
